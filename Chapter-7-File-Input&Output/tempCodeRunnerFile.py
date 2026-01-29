a34bcba06aebd77affb1c7b89a9519ad7d09dea1
EshwarNaikG <eshwarnaik79@gmail.com>
2026-01-29 14:34:43 +0530

    create python notes and interviews Q&A

--- Chapter-7-File-Input&Output/tempCodeRunnerFile.py
+++ Chapter-7-File-Input&Output/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
 
-	def account_info(self):
-	    print(f"account number {self.account_number}, account holder name: {self.account_holder}, balance: {self.balance} ")
+def account_info(self):
+	print(f"account number {self.account_number}, account holder name: {self.account_holder}, balance: {self.balance} ")
 bank_account1 = Bank_Account(1234567, "Eshwara Naik G", 100000)
 bank_account1.account_info()
