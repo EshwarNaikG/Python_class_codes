3bc04e60793d8c29b1ef99b4298a78d27f81c838
EshwarNaikG <eshwarnaik79@gmail.com>
2026-02-18 01:16:37 +0530

    python codes

--- Python_Interviews_Code_Practice_Notes/Day-5-Numbers-Checking/Find_Largest_Number.py
+++ Python_Interviews_Code_Practice_Notes/Day-5-Numbers-Checking/Find_Largest_Number.py
@@ -1,14 +1,16 @@
+# Find largest number among three numbers:-
 a = 10
 b = 50
 c = 30
 
 if (a > b) and (a > c):
-    print(a, "is a greatest number")
+    print(a, "is a largest number")
 
 elif (b > a) and (b > c):
-    print(b, "is a greatest number")
+    print(b, "is a largest number")
 
 else:
-    print(c, "is a greatest number")
+    print(c, "is a largest number")
 
-# Find greatest number from user input values
+# Find largest number from user input values :-
+
